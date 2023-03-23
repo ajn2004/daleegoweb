4399279b8deafadcbfde8e5b82feac29b1ede5b8
Andrew Nelson <andrew@kali.daleego>
2023-03-23 15:51:12 -0400

    Nailed the gallery/project fade transition

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -3,6 +3,7 @@ import { Project } from '../project';
 import Gallery from './Gallery';
 import Focus from './Focus';
 import styles from '@/styles/Home.module.css';
+import { isUint16Array } from 'util/types';
 interface ProjectsProps {
   projects: Project[];
 }
@@ -17,26 +18,41 @@ const Projects: React.FC<ProjectsProps> = ({ projects }) => {
   // keep track of which project is 'in focus'
   const [mode, setMode] = useState<Mode>(Mode.Gallery);
   const [focusedProject, setFocusedProject] = useState<Project | null>(null);
+  const [fade, setFade] = useState<boolean>(false);
+
+  
   const focusProject = (project: Project) => {
-    setMode(Mode.Focus);
-    setFocusedProject(project);
+    setFade(true)
+    setTimeout(() =>{
+      setMode(Mode.Focus);
+      setFocusedProject(project);
+      setFade(false)
+    }, 250)
   };
 
   // allow us to return to the gallery from a project
   const returnToGallery = () => {
-    setMode(Mode.Gallery);
-  };
+    setFade(true)
+    setTimeout(() =>{
+      setMode(Mode.Gallery);
+      setFade(false)
+    }, 250)  };
 
   return (
     <div
     className={styles.gallery_glass}
+    style={{
+      opacity: fade? 0: 1,
+      transition: `opacity 0.25s`,
+    }}
       >
       {mode == Mode.Gallery? (
     <Gallery projects={projects} focusProject={focusProject}/>
       ):(
     <Focus returnToGallery={returnToGallery}/>
       )}
-  </div>
+    </div>
+      
   );
 };
 export default Projects;
