3c4be16b36c44de36047a938b7153ae4d4f66c2a
Andrew Nelson <andrew@kali.daleego>
2023-03-25 11:59:29 -0400

    Edited point clouds to have sphere renders

--- components/projects/components/VRMolecules/basicScene.tsx
+++ components/projects/components/VRMolecules/basicScene.tsx
@@ -1,5 +1,40 @@
+// This file is the basic scene to show us how we can get up and started
+// we're ging to want a couple of different versions here to 
+
 import * as THREE from 'three';
 import { useState, useRef, useEffect } from 'react';
+
+// custom shaders
+const vertexShader = `
+  uniform float size;
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_PointSize = size * (300.0 / -mvPosition.z);
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
+
+const fragmentShader = `
+  uniform vec3 color;
+  varying vec2 vUv;
+
+  void main() {
+    vec2 uv = gl_PointCoord.xy - vec2(0.5, 0.5);
+    float radius = 0.5;
+    float distanceFromCenter = length(uv);
+    float circle = smoothstep(radius, radius + 0.01, distanceFromCenter);
+    gl_FragColor = vec4(color, 1.0 - circle);
+  }
+`;
+
+interface pointProps{
+  inputArray: number[],
+  color: number,
+  radius: number,
+}
 
 const BasicScene = () => {
   const [scene, setScene] = useState<THREE.Scene>();
@@ -19,7 +54,9 @@ const BasicScene = () => {
 
     // Create the camera
     const camera = new THREE.PerspectiveCamera(75, renderWidth / renderHeight, 0.1, 1000);
-    camera.position.z = 10;
+    // camera.position.z = 10;
+    camera.position.z = 40;
+    camera.lookAt(0, 0, 0);
     setCamera(camera);
     // Create the renderer
 
@@ -27,18 +64,59 @@ const BasicScene = () => {
     rendererInstance.setSize(renderWidth, renderHeight);
     renderContainer.appendChild(rendererInstance.domElement);
     setRenderer(rendererInstance);
+    // let's build a function to get pc1 given point array, radius and
 
+
+    
     // Create the point clouds
-    const pc1Geometry = new THREE.BufferGeometry();
-    pc1Geometry.setAttribute('position', new THREE.Float32BufferAttribute([0,0,0, 0,0,1, 0,0,2, 0,1,2, 0,2,2], 3));
-    const pc1Material = new THREE.PointsMaterial({ color: 0xff0000 });
-    const pc1 = new THREE.Points(pc1Geometry, pc1Material);
+    function getPoints({inputArray, color = 0xFF0000, radius = 5}: pointProps) {
+      const pcGeometry = new THREE.BufferGeometry();
+      pcGeometry.setAttribute('position', new THREE.Float32BufferAttribute(inputArray, 3));
+      const pcMaterial = new THREE.ShaderMaterial({
+	uniforms: {
+	  color: { value: new THREE.Color(color) },
+	  size: { value: radius },
+	},
+      vertexShader: vertexShader,
+	fragmentShader: fragmentShader,
+	transparent: true,
+	depthTest: true,
+	depthWrite: false,
+	blending: THREE.AdditiveBlending,
+      });
+      return new THREE.Points(pcGeometry, pcMaterial);
+    };
+    
+    // const pcGeometry = new THREE.BufferGeometry();
+    // pcGeometry.setAttribute('position', new THREE.Float32BufferAttribute([0,0,0, 1,0,1, 1,0,2, 2,1,2, 3,2,2], 3));
+    // const pc1Material = new THREE.ShaderMaterial({
+    //   uniforms: {
+    // 	color: { value: new THREE.Color(0xff0000) },
+    // 	size: { value: 0.1 },
+    //   },
+    //   vertexShader: vertexShader,
+    //   fragmentShader: fragmentShader,
+    //   transparent: true,
+    //   depthTest: true,
+    //   depthWrite: false,
+    //   blending: THREE.AdditiveBlending,
+    // });
+
+    // const pc1 = new THREE.Points(pc1Geometry, pc1Material);
+    const pc1 = getPoints({
+      inputArray : [0,0,0, 1,0,1, 1,0,2, 2,1,2, 3,2,2],
+      color : 0xFF0000,
+      radius:5})
     scene.add(pc1);
 
-    const pc2Geometry = new THREE.BufferGeometry();
-    pc2Geometry.setAttribute('position', new THREE.Float32BufferAttribute([0,0,0, 0,1,0, 0,2,0, 0,2,1, 0,2,2], 3));
-    const pc2Material = new THREE.PointsMaterial({ color: 0x00ff00 });
-    const pc2 = new THREE.Points(pc2Geometry, pc2Material);
+    // const pc2Geometry = new THREE.BufferGeometry();
+    // pc2Geometry.setAttribute('position', new THREE.Float32BufferAttribute([-1,0,0, -1,1,0, -1,2,0, -2,2,1, -3,2,2], 3));
+    // const pc2Material = new THREE.PointsMaterial({ color: 0x00ff00 });
+    // const pc2 = new THREE.Points(pc2Geometry, pc2Material);
+    const pc2 = getPoints({
+      inputArray : [-1,0,0, -1,1,0, -1,2,0, -2,2,1, -3,2,2],
+      color : 0x00FF00,
+      radius:3})
     scene.add(pc2);
 
     // Create a point light and add it to the scene
